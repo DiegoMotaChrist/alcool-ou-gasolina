7b7b9b79e755b78127b2227ee20d198b88afdef0
Diego Mota Christ <diegomota_6@hotmail.com>
2020-05-31 16:48:36 -0300

    init

---
+++ lib/pages/home.page.dart
@@ -0,0 +1,87 @@
+import 'package:aog/widget/logo.widget.dart';
+import 'package:aog/widget/submit-form.widget.dart';
+import 'package:aog/widget/success.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _color = Colors.deepPurple;
+  var _gasCtrl = new MoneyMaskedTextController();
+  var _alcCtrl = new MoneyMaskedTextController();
+  var _busy = false;
+  var _completed = false;
+  var _resultText = "Compensa utilizar alcool";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AnimatedContainer(
+        duration: Duration(
+          microseconds: 1200,
+        ),
+        color: _color,
+        child: ListView(
+          children: <Widget>[
+            Logo(),
+            _completed
+                ? Success(
+                    reset: reset,
+                    result: _resultText,
+                  )
+                : SubmitForm(
+                    alcCtrl: _alcCtrl,
+                    gasCtrl: _gasCtrl,
+                    submitFunc: calculate,
+                    busy: _busy,
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future calculate() {
+    double alc =
+        double.parse(_alcCtrl.text.replaceAll(new RegExp(r'[,.]'), '')) / 100;
+
+    double gas =
+        double.parse(_gasCtrl.text.replaceAll(new RegExp(r'[,.]'), '')) / 100;
+
+    double res = alc / gas;
+
+    setState(() {
+      _color = Colors.deepPurpleAccent as MaterialColor;
+      _completed = false;
+      _busy = true;
+    });
+
+    return new Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        if (res >= 0.7) {
+          _resultText = "Compensa utilizar Gasolina";
+        } else {
+          _resultText = "Compensa ultilizar Álcool";
+        }
+
+        _completed = false;
+        _busy = true;
+      });
+    });
+  }
+
+  reset() {
+    setState(() {
+      _gasCtrl = new MoneyMaskedTextController();
+      _alcCtrl = new MoneyMaskedTextController();
+      _completed = false;
+      _busy = false;
+      _color = Colors.deepPurple;
+    });
+  }
+}
